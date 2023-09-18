15ade234cd3f14b6e6c7e2b5f6b2885998ed76da
Bruna <97135745+bruna-crist@users.noreply.github.com>
2023-09-17 21:29:37 -0300

    Testando o Primeiro Commit.

---
+++ AppJokenpo/app/src/main/java/devandroid/bruna/appjokenpo/MainActivity.java
@@ -0,0 +1,14 @@
+package devandroid.bruna.appjokenpo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
